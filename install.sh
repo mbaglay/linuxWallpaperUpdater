9d548fdee5e5d118a9c228cac61c169ebc9b29c1
Maxim Baglay <mbaglay@productengine.com>
2016-01-20 15:51:43 +0200

    Code updates: 1. Added running updater every 1 hour 2. Updated installer script to copy only files

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ COMMAND_TO_RUN_UPDATER="bash $DIRECTORY_TO_INSTALL/run_updater.sh"
 
 mkdir -p $DIRECTORY_TO_INSTALL
 
-cp -a . $DIRECTORY_TO_INSTALL/.
+for file in `ls -p`
+do
+    cp $file $DIRECTORY_TO_INSTALL/$file
+done
 
 if grep -Fxq "$COMMAND_TO_RUN_UPDATER" $PROFILE
 then
--- run_updater.sh
+++ run_updater.sh
@@ -3,9 +3,14 @@
 DIRECTORY_TO_WORK="$HOME/.wallpaper_updater"
 LOGGER_FILE="RUNNER_LOG"
 
+SCRIPT_NAME=$(basename $0)
+FULL_NAME=$DIRECTORY_TO_WORK/$SCRIPT_NAME
+
 cd $DIRECTORY_TO_WORK
 
 pwd > $LOGGER_FILE
 date >> $LOGGER_FILE
 
 python $DIRECTORY_TO_WORK/wallpaper_updater.py & >> $LOGGER_FILE
+
+echo "bash $FULL_NAME" | at now + 1 hour
