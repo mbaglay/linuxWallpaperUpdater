44e1bac3351afda7a0528ec717f80253b5a094a4
Maxim Baglay <mbaglay@productengine.com>
2016-02-10 17:49:05 +0200

    Added starting of script just after installing. There is ability to avoid starting with using parameter -do-not-run

--- install.sh
+++ install.sh
@@ -3,6 +3,22 @@
 DIRECTORY_TO_INSTALL="$HOME/.wallpaper_updater"
 PROFILE="$HOME/.profile"
 COMMAND_TO_RUN_UPDATER="bash $DIRECTORY_TO_INSTALL/run_updater.sh"
+RUN_AFTER_INSTALL=true
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+
+    case $key in
+        --do-not-run)
+            RUN_AFTER_INSTALL=false
+        ;;
+        *)
+            # Unknown option
+        ;;
+    esac
+    shift
+done
 
 mkdir -p $DIRECTORY_TO_INSTALL
 
@@ -17,4 +33,11 @@ then
 else
     echo "" >> $PROFILE
     echo $COMMAND_TO_RUN_UPDATER >> $PROFILE
+fi
+
+if [ $RUN_AFTER_INSTALL = true ]; then
+    cd $DIRECTORY_TO_INSTALL
+    $COMMAND_TO_RUN_UPDATER
+
+    echo "Script started to work."
 fi
